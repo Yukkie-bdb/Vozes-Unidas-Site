02594e15bde351e85ba06e4f21b374c84217872f
Henrique Checco <checcohenrique@gmail.com>
2024-08-08 08:49:21 -0300

    InserindoTrabalho

---
+++ Vozes-Unidas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vozes_Unidas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
